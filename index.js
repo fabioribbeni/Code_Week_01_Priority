46276e32fb371d2ee4bf39802d16d2658a7bc490
fabioribbeni <fabio.ribbeni@libero.it>
2021-02-15 13:47:27 +0100

    Dati flitrati ed ordinati per priority - Aggiunti alle releative sezioni

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
+let main = document.querySelector("main")
+let firstTier = document.querySelector(".firstTier");
+let secondTier = document.querySelector(".secondTier");
+let thirdTier = document.querySelector(".thirdTier");
+let firstUL = document.querySelector(".firstUL");
+let secondUL = document.querySelector(".secondUL");
+let thirdUL = document.querySelector(".thirdUL");
+let btn = document.querySelector("#favorite")
+let userList = [];
+
+//Carico i dati dall'API
 async function loadData() {
-    let userList = [];
     const response = await fetch("https://jsonplaceholder.typicode.com/todos");
     const users = await response.json();
+    //Aggiungo il parametro priority
     userList = users.map((user) => {
         return {
             ...user,
             priority: Math.floor(Math.random() * 6)
         }
     });
-    console.log(userList)
+    //Ordino i dati per priorità
+    userList.sort(function (a, b) {
+        return b.priority - a.priority;
+    });
+    // Filtro i dati e li metto nei giusti div
+    let priorityHigh = userList.filter((users) => users.priority > 3);
+    let priorityMedium = userList.filter((users) => users.priority > 1 && users.priority < 4);
+    let priorityLow = userList.filter((users) => users.priority < 2);
+
+    priorityHigh.forEach(user => {
+        let userCard = document.createElement("div");
+        userCard.classList.add("userCard");
+        let testo = userCard.textContent = `Priorità: ${user.priority} - ID :${user.id} - Descrizione: ${user.title}`;
+        firstTier.appendChild(userCard);
+    });
+    priorityMedium.forEach(user => {
+        let userCard = document.createElement("div");
+        userCard.classList.add("userCard");
+        let testo = userCard.textContent = `Priorità: ${user.priority} - ID :${user.id} - Descrizione: ${user.title}`;
+        secondTier.appendChild(userCard);
+    });
+    priorityLow.forEach(user => {
+        let userCard = document.createElement("div");
+        userCard.classList.add("userCard");
+        let testo = userCard.textContent = `Priorità: ${user.priority} - ID :${user.id} - Descrizione: ${user.title}`;
+        thirdTier.appendChild(userCard);
+
+    });
 }
 
 loadData();
